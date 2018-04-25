7db55897c5634cddbc87d3bfb1c6e563ac60b53a
Chris Ballinger <chris.ballinger@raizlabs.com>
2018-04-25 15:55:13 -0700

    Update formula

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -2,8 +2,8 @@ class Xchtmlreport < Formula
   desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
   homepage "https://github.com/Raizlabs/XCTestHTMLReport"
   url "https://github.com/Raizlabs/XCTestHTMLReport/archive/1.5.0-raizlabs.zip"
-  sha256 "fe142145e3d84c2416b7614044b0b3cf1bb3a8c2e02ab7c48347063e7cd02497"
-  head "https://github.com/Raizlabs/XCTestHTMLReport.git"
+  sha256 "801cd9fff6887a0c209e85766917c4da05e6a3e519439758c21cd37e0c6ec6fc"
+  head "https://github.com/Raizlabs/XCTestHTMLReport.git", :using => :git, :branch => '1.5.0-raizlabs'
 
   def install
     system "xcodebuild clean build CODE_SIGN_IDENTITY=\"\" CODE_SIGNING_REQUIRED=NO -workspace XCTestHTMLReport.xcworkspace -scheme XCTestHTMLReport -configuration Release"
